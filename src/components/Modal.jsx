b2ad3f5e468e6b163b694ab3931f54684f15d906
Amin <AminMohammadi.dev@gmail.com>
2024-03-28 23:23:57 +0330

    project management app

---
+++ src/components/Modal.jsx
@@ -0,0 +1,25 @@
+import { ForwardedRef, forwardRef, useImperativeHandle, useRef } from "react";
+
+import Button from "./Button.jsx";
+
+const Modal = forwardRef(function Modal({ children, buttonCaption }, ref) {
+  const dialog = useRef();
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        dialog.current.showModal();
+      },
+    };
+  });
+
+  return (
+    <dialog ref={dialog} className="backdrop:bg-stone-900/90 p-4 rounded-md shadow-md">
+      {children}
+      <form method="dialog" className="mt-4 text-right">
+        <Button>{buttonCaption}</Button>
+      </form>
+    </dialog>
+  );
+});
+
+export default Modal;
